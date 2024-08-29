7c2038471f96e1638d41ce724f72dec8f8ccfbb1
xKuth <luisyeager20@gmail.com>
2024-08-29 16:51:41 -0300

    Atualizando a interatividade
    Atualizando a interatividade JS e alguns estilos CSS

--- menu.js
+++ menu.js
@@ -1,13 +1,25 @@
 let btnMenu = document.getElementById('btn-menu');
 let menu = document.getElementById('menu-mobile');
 let overlay = document.getElementById('overlay-menu')
+let rolaralto = document.getElementById('rolaralto')
+let rolarmeio = document.getElementById('rolarmeio')
+let rolarbaixo = document.getElementById('rolarbaixo')
 btnMenu.addEventListener('click', ()=>{
     menu.classList.add('abrir-menu')
-})
+});
 menu.addEventListener('click', ()=>{
     menu.classList.remove('abrir-menu')
-})
+});
 overlay.addEventListener('click', ()=>{
     menu.classList.remove('abrir-menu')
-})
+});
+function rolarpg1(){
+    rolaralto.scrollIntoView({behavior:"smooth"})
+}
+function rolarpg2(){
+    rolarmeio.scrollIntoView({behavior:"smooth"})
+}
+function rolarpg3(){
+    rolarbaixo.scrollIntoView({behavior:"smooth"})
+}
 
